0731d39f4fb0633683f11ea6a7093f74a15992f5
Kelly <kelly85722@gmail.com>
2023-09-14 15:10:35 +0800

    [create] 1. create component BarcodeScanner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useZxing } from 'react-zxing';
+import { useState } from 'react';
+
+function BarcodeScanner() {
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    }
+  })
+
+  return(
+    <>
+      <video ref={ref} />
+      <p>
+        <span>Last result:</span>
+        <span>{result}</span>
+      </p>
+    </>
+  )
+}
 
 function App() {
   return (
@@ -18,6 +39,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <BarcodeScanner />
+      </div>
     </div>
   );
 }
